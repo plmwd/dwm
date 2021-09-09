ce08d9c16780156c3642211c9d1a56e0c3cbf745
Paul Wood <pmwood@pm.me>
2021-09-09 08:08:01 -0400

    colorscheme(borders): Changed border color to be palenight-yellow

--- config.h
+++ config.h
@@ -21,8 +21,8 @@ static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#005577";
-static char selbgcolor[]            = "#005577";
+static char selbordercolor[]        = "#FFCB6B";
+static char selbgcolor[]            = "#FFCB6B";
 static char *colors[][3] = {
        /*               fg           bg           border   */
        [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
